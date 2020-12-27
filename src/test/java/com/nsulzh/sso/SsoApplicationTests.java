bc6a8baf7b8174dd005cbedae23372ff28acf9ad
luozhihui <nsulzh1997>
2020-12-27 23:38:53 +0800

    初次提交

---
+++ src/test/java/com/nsulzh/sso/SsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsulzh.sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
